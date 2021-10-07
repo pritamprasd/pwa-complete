83f50cbc38cad645957660f7cde1bfe0e215510e
Pritam Prasad <pritam.prasd@gmail.com>
2021-10-07 23:56:39 +0530

    working version without workbox

---
+++ public/serviceWorker.js
@@ -0,0 +1,43 @@
+var doCache = true;
+var CACHE_NAME = 'pp-cache-v1';
+
+self.addEventListener("activate", event => {
+    const cacheWhitelist = [CACHE_NAME];
+    event.waitUntil(caches.keys().then(keyList =>
+        Promise.all(keyList.map(key => {
+            if (!cacheWhitelist.includes(key)) {
+                console.log('Deleting cache: ' + key)
+                return caches.delete(key);
+            }
+        }))
+    ));
+});
+
+// The first time the user starts up the PWA, 'install' is triggered.
+self.addEventListener('install', function(event) {
+    if (doCache) {
+        event.waitUntil(caches.open(CACHE_NAME)
+            .then(function(cache) {
+                fetch("asset-manifest.json")
+                    .then(response => response.json())
+                    .then(assets => {
+                        const urlsToCache = [
+                            "/",
+                            assets["main.js"]
+                        ]
+                        cache.addAll(urlsToCache)
+                    })
+            })
+        );
+    }
+});
+
+self.addEventListener('fetch', function(event) {
+    if (doCache) {
+        event.respondWith(
+            caches.match(event.request).then(function(response) {
+                return response || fetch(event.request);
+            })
+        );
+    }
+});
