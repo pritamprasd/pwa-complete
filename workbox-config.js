f65110e6bd200358d726cf8d584f3a462a22aa64
Pritam Prasad <pritam.prasd@gmail.com>
2021-10-08 00:23:37 +0530

    working pwa with workbox

---
+++ workbox-config.js
@@ -0,0 +1,11 @@
+module.exports = {
+	globDirectory: 'build/',
+	globPatterns: [
+		'**/*.{json,ico,html,png,txt,css,js}'
+	],
+	ignoreURLParametersMatching: [
+		/^utm_/,
+		/^fbclid$/
+	],
+	swDest: 'build/sw.js'
+};
